cefaeed9a530fb100e2c7327e859fe6559913aea
QuiqueGueCa <enrique.guevara@vasscompany.com>
2024-03-01 11:45:04 +0100

    added adapter to MainFragment

--- app/src/main/java/com/example/retrofitparallelcoroutines/ui/main_fragment/MainFragment.kt
+++ app/src/main/java/com/example/retrofitparallelcoroutines/ui/main_fragment/MainFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.retrofitparallelcoroutines.databinding.FragmentMainBinding
+import com.example.retrofitparallelcoroutines.ui.main_fragment.adapter.UsersAdapter
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), UsersAdapter.UserListener {
 
     private lateinit var mBinding: FragmentMainBinding
+    private lateinit var mAdapter: UsersAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,6 +21,21 @@ class MainFragment : Fragment() {
 
         mBinding = FragmentMainBinding.inflate(inflater, container, false)
 
+        setupAdapter()
+
         return mBinding.root
+    }
+
+    private fun setupAdapter() {
+        mAdapter = UsersAdapter(this, mutableListOf())
+        with(mBinding.recyclerView) {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = mAdapter
+        }
+    }
+
+    override fun onUserClick(idUser: Int) {
+        // TODO: Aquí iría la magia
     }
 }
---
+++ app/src/main/java/com/example/retrofitparallelcoroutines/ui/main_fragment/adapter/UsersAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.retrofitparallelcoroutines.ui.main_fragment.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.retrofitparallelcoroutines.R
+import com.example.retrofitparallelcoroutines.data.domain.model.user.UserModel
+import com.example.retrofitparallelcoroutines.databinding.ItemRecyclerUserBinding
+
+class UsersAdapter(
+    private val listener: UserListener,
+    private val users: MutableList<UserModel> = mutableListOf()
+) : RecyclerView.Adapter<UsersAdapter.ViewHolder>() {
+
+    interface UserListener {
+        fun onUserClick(idUser: Int)
+    }
+
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val binding = ItemRecyclerUserBinding.bind(view)
+        fun setListener(idUser: Int) {
+            binding.root.setOnClickListener {
+                listener.onUserClick(idUser)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_recycler_user, parent, false
+        )
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        with(holder.binding) {
+            tvIdContent.text = users[position].id.toString()
+            tvNameContent.text = users[position].name
+        }
+        holder.setListener(users[position].id)
+    }
+
+    override fun getItemCount(): Int = users.count()
+}
