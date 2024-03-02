6ae755db7c053ef2faf6851ad7a39a74909084a3
QuiqueGueCa <enrique.guevara@vasscompany.com>
2024-03-02 20:37:49 +0100

    added coroutines on parallel

--- app/src/main/java/com/example/retrofitparallelcoroutines/ui/main_fragment/MainFragment.kt
+++ app/src/main/java/com/example/retrofitparallelcoroutines/ui/main_fragment/MainFragment.kt
@@ -8,7 +8,11 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.retrofitparallelcoroutines.data.domain.model.payroll.PayrollModel
+import com.example.retrofitparallelcoroutines.data.domain.use_cases.GetJobUseCase
 import com.example.retrofitparallelcoroutines.data.domain.use_cases.GetNamesListUseCase
+import com.example.retrofitparallelcoroutines.data.domain.use_cases.GetSalaryUseCase
+import com.example.retrofitparallelcoroutines.data.domain.use_cases.GetSurnameUseCase
 import com.example.retrofitparallelcoroutines.databinding.FragmentMainBinding
 import com.example.retrofitparallelcoroutines.ui.main_fragment.adapter.UsersAdapter
 import kotlinx.coroutines.launch
@@ -17,7 +21,12 @@ class MainFragment : Fragment(), UsersAdapter.UserListener {
 
     private lateinit var mBinding: FragmentMainBinding
     private lateinit var mAdapter: UsersAdapter
-    private val mViewModel = MainFragmentViewModel(GetNamesListUseCase())
+    private val mViewModel = MainFragmentViewModel(
+        GetNamesListUseCase(),
+        GetSurnameUseCase(),
+        GetJobUseCase(),
+        GetSalaryUseCase(),
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,7 +51,11 @@ class MainFragment : Fragment(), UsersAdapter.UserListener {
             }
         }
 
-
+        lifecycleScope.launch {
+            mViewModel.payrollStateFlow.collect { dataSet ->
+                setInformationAtUi(dataSet)
+            }
+        }
 
         lifecycleScope.launch {
             mViewModel.namesListErrorSharedFlow.collect { error ->
@@ -52,6 +65,43 @@ class MainFragment : Fragment(), UsersAdapter.UserListener {
                     Toast.LENGTH_SHORT
                 ).show()
             }
+        }
+        lifecycleScope.launch {
+            mViewModel.surnameErrorSharedFlow.collect { error ->
+                Toast.makeText(
+                    requireContext(),
+                    error.message,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        lifecycleScope.launch {
+            mViewModel.jobErrorSharedFlow.collect { error ->
+                Toast.makeText(
+                    requireContext(),
+                    error.message,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        lifecycleScope.launch {
+            mViewModel.salaryErrorSharedFlow.collect { error ->
+                Toast.makeText(
+                    requireContext(),
+                    error.message,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun setInformationAtUi(dataSet: PayrollModel) {
+        with(mBinding) {
+            tvIdContent.text = dataSet.id.toString()
+            tvNameContent.text = dataSet.name
+            tvSurnameContent.text = dataSet.surname
+            tvCompanyContent.text = dataSet.company
+            tvSalaryContent.text = dataSet.salary.toString()
         }
     }
 
@@ -65,6 +115,6 @@ class MainFragment : Fragment(), UsersAdapter.UserListener {
     }
 
     override fun onUserClick(idUser: Int) {
-        // TODO: Aquí iría la magia
+        mViewModel.getPayroll(idUser)
     }
 }
--- app/src/main/java/com/example/retrofitparallelcoroutines/ui/main_fragment/MainFragmentViewModel.kt
+++ app/src/main/java/com/example/retrofitparallelcoroutines/ui/main_fragment/MainFragmentViewModel.kt
@@ -2,11 +2,22 @@ package com.example.retrofitparallelcoroutines.ui.main_fragment
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.retrofitparallelcoroutines.data.domain.model.BaseModel
 import com.example.retrofitparallelcoroutines.data.domain.model.error.ErrorModel
+import com.example.retrofitparallelcoroutines.data.domain.model.payroll.JobModel
 import com.example.retrofitparallelcoroutines.data.domain.model.payroll.NameModel
+import com.example.retrofitparallelcoroutines.data.domain.model.payroll.PayrollModel
+import com.example.retrofitparallelcoroutines.data.domain.model.payroll.SalaryModel
+import com.example.retrofitparallelcoroutines.data.domain.model.payroll.SurnameModel
 import com.example.retrofitparallelcoroutines.data.domain.repository.remote.response.BaseResponse
+import com.example.retrofitparallelcoroutines.data.domain.use_cases.GetJobUseCase
 import com.example.retrofitparallelcoroutines.data.domain.use_cases.GetNamesListUseCase
+import com.example.retrofitparallelcoroutines.data.domain.use_cases.GetSalaryUseCase
+import com.example.retrofitparallelcoroutines.data.domain.use_cases.GetSurnameUseCase
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
@@ -14,17 +25,37 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class MainFragmentViewModel(
-    private val getNamesListUseCase: GetNamesListUseCase
+    private val getNamesListUseCase: GetNamesListUseCase,
+    private val getSurnameUseCase: GetSurnameUseCase,
+    private val getJobUseCase: GetJobUseCase,
+    private val getSalaryUseCase: GetSalaryUseCase
 ) : ViewModel() {
 
     private val namesListErrorMutableSharedFlow = MutableSharedFlow<ErrorModel>()
     val namesListErrorSharedFlow: SharedFlow<ErrorModel> = namesListErrorMutableSharedFlow
 
+    private val surnameErrorMutableSharedFlow = MutableSharedFlow<ErrorModel>()
+    val surnameErrorSharedFlow: SharedFlow<ErrorModel> = surnameErrorMutableSharedFlow
+
+    private val jobErrorMutableSharedFlow = MutableSharedFlow<ErrorModel>()
+    val jobErrorSharedFlow: SharedFlow<ErrorModel> = jobErrorMutableSharedFlow
+
+    private val salaryErrorMutableSharedFlow = MutableSharedFlow<ErrorModel>()
+    val salaryErrorSharedFlow: SharedFlow<ErrorModel> = salaryErrorMutableSharedFlow
+
+
     private val namesListMutableStateFlow =
         MutableStateFlow<MutableList<NameModel>>(mutableListOf())
     val namesListStateFlow: StateFlow<MutableList<NameModel>> = namesListMutableStateFlow
 
+    private val payrollMutableStateFlow = MutableStateFlow(PayrollModel())
+    val payrollStateFlow: StateFlow<PayrollModel> = payrollMutableStateFlow
+
     private val names = mutableListOf<NameModel>()
+    private var surnameModel = SurnameModel()
+    private var jobModel = JobModel()
+    private var salaryModel = SalaryModel()
+    //private var payrollModel = PayrollModel()
 
     fun getNamesList() {
         viewModelScope.launch(Dispatchers.IO) {
@@ -37,8 +68,71 @@ class MainFragmentViewModel(
                     is BaseResponse.Success -> {
                         names.addAll(it.data.names)
                         namesListMutableStateFlow.value = names
-
                     }
+                }
+            }
+        }
+    }
+
+    fun getPayroll(idUser: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val deferreds = listOf(
+                async { getSurnameUseCase.invoke(idUser) },
+                async { getJobUseCase.invoke(idUser) },
+                async { getSalaryUseCase.invoke(idUser) }
+            )
+            val response = deferreds.awaitAll()
+            getSurname(response[0])
+            getJob(response[1])
+            getSalary(response[2])
+
+            payrollMutableStateFlow.value = PayrollModel(
+                idUser,
+                surnameModel.name,
+                surnameModel.surname,
+                jobModel.company,
+                salaryModel.salary
+            )
+        }
+    }
+
+    private suspend fun getSalary(flow: Flow<BaseResponse<out BaseModel>>) {
+        flow.collect {
+            when (it) {
+                is BaseResponse.Error -> {
+                    salaryErrorMutableSharedFlow.emit(it.error)
+                }
+
+                is BaseResponse.Success -> {
+                    salaryModel = it.data as SalaryModel
+                }
+            }
+        }
+    }
+
+    private suspend fun getJob(flow: Flow<BaseResponse<out BaseModel>>) {
+        flow.collect {
+            when (it) {
+                is BaseResponse.Error -> {
+                    jobErrorMutableSharedFlow.emit(it.error)
+                }
+
+                is BaseResponse.Success -> {
+                    jobModel = it.data as JobModel
+                }
+            }
+        }
+    }
+
+    private suspend fun getSurname(flow: Flow<BaseResponse<out BaseModel>>) {
+        flow.collect {
+            when (it) {
+                is BaseResponse.Error -> {
+                    surnameErrorMutableSharedFlow.emit(it.error)
+                }
+
+                is BaseResponse.Success -> {
+                    surnameModel = it.data as SurnameModel
                 }
             }
         }
