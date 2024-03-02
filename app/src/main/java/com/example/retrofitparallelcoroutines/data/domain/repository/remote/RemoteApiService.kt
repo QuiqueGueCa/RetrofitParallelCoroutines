2bf2d0ad59dbeac9f2e41d651b8e03e6933fd1cb
QuiqueGueCa <enrique.guevara@vasscompany.com>
2024-03-02 17:10:17 +0100

    added funs to RemoteApiService

--- app/src/main/java/com/example/retrofitparallelcoroutines/data/domain/repository/remote/RemoteApiService.kt
+++ app/src/main/java/com/example/retrofitparallelcoroutines/data/domain/repository/remote/RemoteApiService.kt
@@ -1,15 +1,34 @@
 package com.example.retrofitparallelcoroutines.data.domain.repository.remote
 
+import com.example.retrofitparallelcoroutines.data.domain.repository.remote.response.user.JobResponse
 import com.example.retrofitparallelcoroutines.data.domain.repository.remote.response.user.NamesListResponse
+import com.example.retrofitparallelcoroutines.data.domain.repository.remote.response.user.PayrollResponse
+import com.example.retrofitparallelcoroutines.data.domain.repository.remote.response.user.SalaryResponse
+import com.example.retrofitparallelcoroutines.data.domain.repository.remote.response.user.SurnameResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface RemoteApiService {
     @GET("names")
     suspend fun getNamesList(): Response<NamesListResponse>
 
-    /*@GET("pokemon/{id}")
-    suspend fun getPokemonDetail(
-        @Path("id") idPokemon: Int
-    ): Response<PokemonDetailResponse>*/
+    @GET("surname/{id}")
+    suspend fun getSurname(
+        @Path("id") idUser: Int
+    ): Response<SurnameResponse>
+
+    @GET("job/{id}")
+    suspend fun getJob(
+        @Path("id") idUser: Int
+    ): Response<JobResponse>
+
+    @GET("salary/{id}")
+    suspend fun getSalary(
+        @Path("id") idUser: Int
+    ): Response<SalaryResponse>
+
+    @POST("payroll")
+    suspend fun setPayroll(): Response<PayrollResponse>
 }
