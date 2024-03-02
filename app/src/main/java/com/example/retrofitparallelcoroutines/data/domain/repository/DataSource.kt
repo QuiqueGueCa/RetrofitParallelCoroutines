0d564177a5a883c1699f9b8c0cf9213e28e12324
QuiqueGueCa <enrique.guevara@vasscompany.com>
2024-03-02 17:50:26 +0100

    added getters to DataSource, RemoteDataSource and DataProvider

--- app/src/main/java/com/example/retrofitparallelcoroutines/data/domain/repository/DataSource.kt
+++ app/src/main/java/com/example/retrofitparallelcoroutines/data/domain/repository/DataSource.kt
@@ -1,9 +1,15 @@
 package com.example.retrofitparallelcoroutines.data.domain.repository
 
+import com.example.retrofitparallelcoroutines.data.domain.model.user.JobModel
 import com.example.retrofitparallelcoroutines.data.domain.model.user.NamesListModel
+import com.example.retrofitparallelcoroutines.data.domain.model.user.SalaryModel
+import com.example.retrofitparallelcoroutines.data.domain.model.user.SurnameModel
 import com.example.retrofitparallelcoroutines.data.domain.repository.remote.response.BaseResponse
 import kotlinx.coroutines.flow.Flow
 
 interface DataSource {
     fun getNamesList(): Flow<BaseResponse<NamesListModel>>
+    fun getSurname(idUser: Int): Flow<BaseResponse<SurnameModel>>
+    fun getJob(idUser: Int): Flow<BaseResponse<JobModel>>
+    fun getSalary(idUser: Int): Flow<BaseResponse<SalaryModel>>
 }
--- app/src/main/java/com/example/retrofitparallelcoroutines/data/domain/repository/remote/RemoteDataSource.kt
+++ app/src/main/java/com/example/retrofitparallelcoroutines/data/domain/repository/remote/RemoteDataSource.kt
@@ -1,8 +1,14 @@
 package com.example.retrofitparallelcoroutines.data.domain.repository.remote
 
+import com.example.retrofitparallelcoroutines.data.domain.model.user.JobModel
 import com.example.retrofitparallelcoroutines.data.domain.model.user.NamesListModel
+import com.example.retrofitparallelcoroutines.data.domain.model.user.SalaryModel
+import com.example.retrofitparallelcoroutines.data.domain.model.user.SurnameModel
 import com.example.retrofitparallelcoroutines.data.domain.repository.DataSource
+import com.example.retrofitparallelcoroutines.data.domain.repository.remote.mapper.user.JobMapper
 import com.example.retrofitparallelcoroutines.data.domain.repository.remote.mapper.user.NamesListMapper
+import com.example.retrofitparallelcoroutines.data.domain.repository.remote.mapper.user.SalaryMapper
+import com.example.retrofitparallelcoroutines.data.domain.repository.remote.mapper.user.SurnameMapper
 import com.example.retrofitparallelcoroutines.data.domain.repository.remote.response.BaseResponse
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -12,13 +18,38 @@ object RemoteDataSource : DataSource {
     //Aquí está la variable de la inyección de dependencias
     private val apiCallService = ApiCallService(RetrofitClient.getApiServices())
     override fun getNamesList(): Flow<BaseResponse<NamesListModel>> = flow {
-
         val apiResult = apiCallService.getNamesList()
         if (apiResult is BaseResponse.Success) {
             emit(BaseResponse.Success(NamesListMapper().fromResponse(apiResult.data)))
         } else if (apiResult is BaseResponse.Error) {
             emit(BaseResponse.Error(apiResult.error))
         }
+    }
 
+    override fun getSurname(idUser: Int): Flow<BaseResponse<SurnameModel>> = flow {
+        val apiResult = apiCallService.getSurname(idUser)
+        if (apiResult is BaseResponse.Success) {
+            emit(BaseResponse.Success(SurnameMapper().fromResponse(apiResult.data)))
+        } else if (apiResult is BaseResponse.Error) {
+            emit(BaseResponse.Error(apiResult.error))
+        }
+    }
+
+    override fun getJob(idUser: Int): Flow<BaseResponse<JobModel>> = flow {
+        val apiResult = apiCallService.getJob(idUser)
+        if (apiResult is BaseResponse.Success) {
+            emit(BaseResponse.Success(JobMapper().fromResponse(apiResult.data)))
+        } else if (apiResult is BaseResponse.Error) {
+            emit(BaseResponse.Error(apiResult.error))
+        }
+    }
+
+    override fun getSalary(idUser: Int): Flow<BaseResponse<SalaryModel>> = flow {
+        val apiResult = apiCallService.getSalary(idUser)
+        if (apiResult is BaseResponse.Success) {
+            emit(BaseResponse.Success(SalaryMapper().fromResponse(apiResult.data)))
+        } else if (apiResult is BaseResponse.Error) {
+            emit(BaseResponse.Error(apiResult.error))
+        }
     }
 }
